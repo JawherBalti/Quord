1c96e358a73afaeefd1b3d72b96cd531426cbd18
jawherbalti <jawher-balti@live.com>
2024-12-03 16:29:55 +0100

    add authorization headers to requests

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, map, Observable, throwError } from 'rxjs';
 import { Author } from '../models/author.model';
@@ -20,16 +20,21 @@ export class AuthService {
     this.userDataSubject.next(userData);
   }
 
-  register(author: FormData): Observable<Author> {
+  getAuthHeader(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
+
+  register(author: { name: string, lastName: string, email: string, password: string, about: string, image: string }): Observable<Author> {
     return this.http.post<Author>(this.url + "register", author)
   }
 
-  login(author: {email: string, password: string}): Observable<any> {
+  login(author: { email: string, password: string }): Observable<any> {
     return this.http.post<any>(this.url + "login", author)
   }
 
   update(id: string, author: any) {
-    return this.http.patch(this.url + "update/" + id, author)
+    return this.http.patch(this.url + "update/" + id, author, { headers: this.getAuthHeader() })
   }
 
   isLoggedIn() {
